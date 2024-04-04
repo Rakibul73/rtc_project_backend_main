bb86a4bf5aa314e7c49b5ce976e68fa6346fd701
Rakibul73 <rakibul29185@yahoo.com>
2024-04-05 02:07:06 +0600

    fund admin panle route added , used some join operation on mysql

--- fund.py
+++ fund.py
@@ -20,18 +20,21 @@ def get_my_fund_dashboard():
     total_project_list_can_apply_fund = cursor.fetchall()
     print(total_project_list_can_apply_fund)
     total_project_can_apply_fund = len(total_project_list_can_apply_fund)
-    
-    project_list_can_apply_fund = [project['ProjectID'] for project in total_project_list_can_apply_fund]
-    print(project_list_can_apply_fund)
-    cursor.execute("SELECT COUNT(ProjectID) AS total_project_applied_fund FROM ProjectFund WHERE ProjectID IN ({}) AND RequestForFundDone > 0".format(
-            ', '.join(['%s']*len(project_list_can_apply_fund))), project_list_can_apply_fund)
-    total_project_applied_fund = cursor.fetchone()
-    print(total_project_applied_fund['total_project_applied_fund'])
-    
-    cursor.execute("SELECT COUNT(ProjectID) AS total_project_fund_recieved FROM ProjectFund WHERE ProjectID IN ({}) AND FundSendDone > 0".format(
-            ', '.join(['%s']*len(project_list_can_apply_fund))), project_list_can_apply_fund)
-    total_project_fund_recieved = cursor.fetchone()
-    print(total_project_fund_recieved['total_project_fund_recieved'])
+    if len(total_project_list_can_apply_fund) == 0:
+        total_project_applied_fund = {'total_project_applied_fund': 0}
+        total_project_fund_recieved = {'total_project_fund_recieved': 0}
+    else:
+        project_list_can_apply_fund = [project['ProjectID'] for project in total_project_list_can_apply_fund]
+        print(project_list_can_apply_fund)
+        cursor.execute("SELECT COUNT(ProjectID) AS total_project_applied_fund FROM ProjectFund WHERE ProjectID IN ({}) AND RequestForFundDone > 0".format(
+                ', '.join(['%s']*len(project_list_can_apply_fund))), project_list_can_apply_fund)
+        total_project_applied_fund = cursor.fetchone()
+        print(total_project_applied_fund['total_project_applied_fund'])
+        
+        cursor.execute("SELECT COUNT(ProjectID) AS total_project_fund_recieved FROM ProjectFund WHERE ProjectID IN ({}) AND FundSendDone > 0".format(
+                ', '.join(['%s']*len(project_list_can_apply_fund))), project_list_can_apply_fund)
+        total_project_fund_recieved = cursor.fetchone()
+        print(total_project_fund_recieved['total_project_fund_recieved'])
     
     cursor.close()
     conn.close()
@@ -95,8 +98,16 @@ def get_specific_project_for_fund_self(project_id):
     cursor = conn.cursor(dictionary=True)
     
     current_user_id = get_jwt_identity()
-    cursor.execute("SELECT CodeByRTC , ProjectTitle , TotalBudgetOfResearchProposalTK , CreatorUserSealLocation , CreatorUserSignatureDate , CreatorUserSignatureLocation , ChairmanOfDepartmentSignatureDate , ChairmanOfDepartmentSignatureLocation , ChairmanOfDepartmentSealLocation  FROM Projects WHERE ProjectID = %s AND CreatorUserID = %s", (project_id, current_user_id))
-    project_data = cursor.fetchone()
+    # check if current user role is 1 or not
+    cursor.execute("SELECT RoleID FROM Users WHERE UserID = %s", (current_user_id,))
+    current_user_role = cursor.fetchone()
+    print(current_user_role)
+    if current_user_role['RoleID'] == 1:
+        cursor.execute("SELECT CodeByRTC , ProjectTitle , TotalBudgetOfResearchProposalTK , CreatorUserSealLocation , CreatorUserSignatureDate , CreatorUserSignatureLocation , ChairmanOfDepartmentSignatureDate , ChairmanOfDepartmentSignatureLocation , ChairmanOfDepartmentSealLocation  FROM Projects WHERE ProjectID = %s", (project_id,))
+        project_data = cursor.fetchone()
+    else:
+        cursor.execute("SELECT CodeByRTC , ProjectTitle , TotalBudgetOfResearchProposalTK , CreatorUserSealLocation , CreatorUserSignatureDate , CreatorUserSignatureLocation , ChairmanOfDepartmentSignatureDate , ChairmanOfDepartmentSignatureLocation , ChairmanOfDepartmentSealLocation  FROM Projects WHERE ProjectID = %s AND CreatorUserID = %s", (project_id, current_user_id))
+        project_data = cursor.fetchone()
     cursor.close()
     conn.close()
     if project_data:
@@ -220,5 +231,120 @@ def update_confirm_fund_recieved(project_id):
     conn.close()
     return jsonify({'message': 'Project Fund recieved confirm updated successfully' , 'statuscode' : 200}), 200
 
+
+
+# Route to get total number of all dashboard
+@fund_blueprint.route('/get_admin_fund_dashboard', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1])  
+def get_admin_fund_dashboard():
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+    
+    cursor.execute("SELECT ProjectID FROM ProjectFund WHERE FundSendDone > 0")
+    funded_projects = cursor.fetchall()
+    print(funded_projects)
+    funded_projects = len(funded_projects)
+    
+    cursor.execute("SELECT ProjectID FROM ProjectFund WHERE RequestForFundDone > 0")
+    fund_request_queue = cursor.fetchall()
+    print(fund_request_queue)
+    fund_request_queue = len(fund_request_queue) - funded_projects
+    
+    cursor.execute("SELECT ProjectID FROM ProjectFund WHERE FundRecievedDone > 0")
+    fund_confirmation_recieved = cursor.fetchall()
+    print(fund_confirmation_recieved)
+    fund_confirmation_recieved = len(fund_confirmation_recieved)
+    
+
+    
+    cursor.close()
+    conn.close()
+    
+    return jsonify({
+        'fund_request_queue': fund_request_queue,
+        'funded_projects': funded_projects,
+        'fund_confirmation_recieved': fund_confirmation_recieved,
+        'statuscode' : 200
+    }) , 200
+
+
+
+# Route to get all projects
+@fund_blueprint.route('/get_admin_fund_queue_list', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1])  
+def get_admin_fund_queue_list():
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+    
+    
+    cursor.execute("SELECT * FROM ProjectFund WHERE RequestForFundDone > 0 AND FundSendDone = 0")
+    projects_fund_queue = cursor.fetchall()
+    print(projects_fund_queue)
+    
+    cursor.close()
+    conn.close()
+    
+    return jsonify({'projects_fund_queue': projects_fund_queue  , "statuscode" : 200}) , 200
+
+
+
+
+# Route to get wether a project reviewed or not
+@fund_blueprint.route('/check_a_project_fund_send_or_not/<int:project_id>', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1 , 2 , 3 , 4 , 5])  # Only admin and supervisor can access this route
+def check_a_project_fund_send_or_not(project_id ):
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+    
+    cursor.execute("SELECT ProjectFundID FROM ProjectFund WHERE FundSendDone > 0 AND ProjectID = %s" , (project_id,))
+    ProjectFundSendCheck = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    if ProjectFundSendCheck:
+        return jsonify({'ProjectFundSendCheck': "Yes" , "statuscode" : 200}) , 200
+    else:
+        return jsonify({'ProjectFundSendCheck': "No" , "statuscode" : 200}) , 200
+
+
+
+
+
+# Route to update a specific project fund send FundSendDone 0 = not Received , 1 = 1st Received
+@fund_blueprint.route('/update_fund_send/<int:project_id>', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1, 2 , 3 , 4 , 5])
+def update_fund_send(project_id):
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("UPDATE ProjectFund SET FundSendDone = 1 WHERE ProjectID = %s" , (project_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({'message': 'Project Fund send confirm updated successfully' , 'statuscode' : 200}), 200
+
+
+
+
+# Route to get all projects
+@fund_blueprint.route('/get_admin_fund_confirm_list', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1])  
+def get_admin_fund_confirm_list():
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+    
+    
+    # cursor.execute("SELECT * FROM ProjectFund WHERE FundRecievedDone > 0")
+    cursor.execute("SELECT p.ProjectID, p.ProjectTitle, p.CodeByRTC, pf.HonorariumOfCoPI, pf.HonorariumOfPI , pf.TotalHonorarium , pf.TotalBudget FROM Projects p JOIN ProjectFund pf ON p.ProjectID = pf.ProjectID WHERE pf.FundRecievedDone > 0")
+    projects_fund_confirm = cursor.fetchall()
+    print(projects_fund_confirm)
+    
+    cursor.close()
+    conn.close()
+    
+    return jsonify({'projects_fund_confirm': projects_fund_confirm  , "statuscode" : 200}) , 200
 
 
