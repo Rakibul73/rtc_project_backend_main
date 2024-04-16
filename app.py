6ebd2758df24191db53fd0b5e87db5ad9c0847d3
Rakibul73 <rakibul29185@yahoo.com>
2024-04-16 23:26:01 +0600

    added create notice & view notice routes

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from upload import upload_blueprint
 from download import download_blueprint
 from notification import notification_blueprint
 from fund import fund_blueprint
+from notice import notice_blueprint
 from flask_compress import Compress
 
 
@@ -56,6 +57,9 @@ app.register_blueprint(notification_blueprint)
 
 # Register the Fund blueprint
 app.register_blueprint(fund_blueprint)
+
+# Register the Notice blueprint
+app.register_blueprint(notice_blueprint)
 
 @app.route('/', methods=['GET'])
 def index():
---
+++ notice.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, jsonify, request, send_from_directory
+from flask_jwt_extended import jwt_required
+from auth_utils import role_required
+from db import get_db
+
+notice_blueprint = Blueprint('notice', __name__)
+
+
+# Route to get all notices
+@notice_blueprint.route('/notices', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1 , 2 , 3 , 4 , 5])
+def get_all_projects():
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+    
+    cursor.execute("SELECT * FROM Notice ORDER BY DatePublished DESC")
+    notices_list = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    
+    return jsonify({'notices': notices_list  , "statuscode" : 200}) , 200
+
+
+
+# Route to create a new project
+@notice_blueprint.route('/create_notice', methods=['POST'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1])
+def create_notice():
+    data = request.get_json()
+    conn = get_db()
+    cursor = conn.cursor()
+    
+    insert_query = "INSERT INTO Notice (Subject, NoticeFileLocation) VALUES (%s, %s )"
+    user_data = ( data['Subject'], data['NoticeFileLocation'])
+    cursor.execute(insert_query, user_data)
+    conn.commit()
+    notice_id = cursor.lastrowid
+    cursor.close()
+    conn.close()
+    return jsonify({'message': 'Notice created successfully' , 'notice_id' : notice_id , 'statuscode' : 201}), 201
+
+
+
+
+
+
+
+
+
+
