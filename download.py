45fef117eb0bcf8938a7aa86434abb5dc9590823
Rakibul73 <rakibul29185@yahoo.com>
2024-04-17 03:46:04 +0600

    added download route for notice file

--- download.py
+++ download.py
@@ -45,3 +45,9 @@ def download_nid(filename):
 def download_project_softcopy(filename):
     # Logic for downloading the methodology
     return send_from_directory('upload/project_softcopy', filename)
+
+@download_blueprint.route('/notice/download/<filename>', methods=['GET'])
+@jwt_required()  # Protect the route with JWT
+@role_required([1, 2 , 3 , 4 , 5])
+def download_notice(filename):
+    return send_from_directory('upload/notices', filename)
