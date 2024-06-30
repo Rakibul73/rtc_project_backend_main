95ea7bef49714c209b2950845dbc1a6659e03abc
Rakibul73 <rakibul29185@yahoo.com>
2024-06-30 22:41:18 +0600

    added a db route for webhook to pythonanywhere

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ from monitoring import monitoring_blueprint
 from notice import notice_blueprint
 from flask_compress import Compress
 import git  # GitPython library
+from db import get_db
+from mysql.connector import Error
 
 app = Flask(__name__)
 CORS(app)  # Add this line
@@ -88,6 +90,68 @@ def git_update():
     repo.git.stash()
     # repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
     origin.pull()
+    return '', 200
+
+
+
+def drop_tables(connection, cursor):
+    tables = [
+        "ActivityPlan",
+        "ProjectMonitoringReportBudget",
+        "ProjectMonitoringReportActivity",
+        "BudgetPlanHistory",
+        "ActivityPlanHistory",
+        "ActivityPlanOriginal",
+        "BudgetPlanOriginal",
+        "ProjectReportListWithMonitoringCommitteeID",
+        "ProjectMonitoringFeedback",
+        "ProjectMonitoringReport",
+        "BudgetPlan",
+        "Notification",
+        "PassReset",
+        "ProjectFund",
+        "ProjectAdvanceFund",
+        "ProjectListWithReviewerID",
+        "ProjectListWithUserID",
+        "Review",
+        "TempUsers",
+        "Projects",
+        "Users",
+        "Role",
+        "Notice"
+    ]
+
+    for table in tables:
+        try:
+            cursor.execute(f"DROP TABLE IF EXISTS {table};")
+            print(f"Dropped table {table}")
+        except Error as e:
+            print(f"Error: {e}")
+            connection.rollback()
+
+def import_sql_file(connection, cursor, sql_file_path):
+    try:
+        with open(sql_file_path, 'r') as file:
+            sql_commands = file.read()
+        for command in sql_commands.split(';'):
+            if command.strip():
+                cursor.execute(command)
+        connection.commit()
+        print("SQL file imported successfully")
+    except Error as e:
+        print(f"Error: {e}")
+        connection.rollback()
+    except FileNotFoundError as fnfe:
+        print(f"File not found: {fnfe}")
+
+
+@app.route('/mysql_db_update', methods=['POST'])
+def git_update():
+    sql_file_path = './mysite/rtc_project_backend/pstu_rtc.sql'
+    conn = get_db()
+    cursor = conn.cursor()
+    drop_tables(conn, cursor)
+    import_sql_file(conn, cursor, sql_file_path)
     return '', 200
 
 
