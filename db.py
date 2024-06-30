1266a50a93e01c8227674ac71f1dc6a9331cd75a
Md. Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2024-06-30 22:14:40 +0600

    Updated db config for pythonanywhere

--- db.py
+++ db.py
@@ -1,19 +1,19 @@
 import mysql.connector
 
 # for pythonanywhere server
-# db_config = {
-#     'host': 'rakib73.mysql.pythonanywhere-services.com',
-#     'user': 'rakib73',
-#     'password': 'rakib73rakib73',
-#     'database': 'rakib73$pstu_rtc'
-# }
-
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'pstu_rtc'
+    'host': 'rakib73.mysql.pythonanywhere-services.com',
+    'user': 'rakib73',
+    'password': 'rakib73rakib73',
+    'database': 'rakib73$final_project'
 }
+
+# db_config = {
+#     'host': 'localhost',
+#     'user': 'root',
+#     'password': '',
+#     'database': 'pstu_rtc'
+# }
 
 # Function to establish a database connection
 
