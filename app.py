d38c6b56634d687c3345767f838c8570a4e8f406
Rakibul73 <rakibul29185@yahoo.com>
2024-10-28 15:13:19 +0600

    updated for production

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ from fund_advance import fund_advance_blueprint
 from monitoring import monitoring_blueprint
 from notice import notice_blueprint
 from flask_compress import Compress
-import git  # GitPython library
 from db import get_db
 from mysql.connector import Error
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://pstu-rtc.netlify.app", "http://localhost:5912"]}})
+# CORS(app, resources={r"/*": {"origins": ["https://pstu-rtc.netlify.app", "http://localhost:5912"]}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 Compress(app)
 
 
@@ -78,94 +78,7 @@ app.register_blueprint(notice_blueprint)
 def index():
     return 'Welcome to the PSTU RTC Project Backend API!'
 
-@app.route('/git_update', methods=['POST'])
-def git_update():
-    """
-    Update the git repository with the latest changes from the remote and switch to the main branch.
-    Returns:
-        tuple: A tuple in the format (None, 200) indicating a successful update.
-    """
-    repo = git.Repo('./mysite/rtc_project_backend')
-    origin = repo.remotes.origin
-    # Stash local changes
-    repo.git.stash()
-    # repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
-
-
-
-def drop_tables(connection, cursor):
-    tables = [
-        "ActivityPlan",
-        "ProjectMonitoringReportBudget",
-        "ProjectMonitoringReportActivity",
-        "BudgetPlanHistory",
-        "ActivityPlanHistory",
-        "ActivityPlanOriginal",
-        "BudgetPlanOriginal",
-        "ProjectReportListWithMonitoringCommitteeID",
-        "ProjectMonitoringFeedback",
-        "ProjectMonitoringReport",
-        "BudgetPlan",
-        "Notification",
-        "PassReset",
-        "ProjectFund",
-        "ProjectAdvanceFund",
-        "ProjectListWithReviewerID",
-        "ProjectListWithUserID",
-        "Review",
-        "TempUsers",
-        "Projects",
-        "Users",
-        "Role",
-        "Notice"
-    ]
-
-    for table in tables:
-        try:
-            cursor.execute(f"DROP TABLE IF EXISTS {table};")
-            print(f"Dropped table {table}")
-        except Error as e:
-            print(f"Error: {e}")
-            connection.rollback()
-
-def import_sql_file(connection, cursor, sql_file_path):
-    try:
-        with open(sql_file_path, 'r') as file:
-            sql_commands = file.read()
-        for command in sql_commands.split(';'):
-            if command.strip():
-                cursor.execute(command)
-        connection.commit()
-        print("SQL file imported successfully")
-    except Error as e:
-        print(f"Error: {e}")
-        connection.rollback()
-    except FileNotFoundError as fnfe:
-        print(f"File not found: {fnfe}")
-
-def show_tables(connection, cursor):
-    tables = []
-    try:
-        cursor.execute("SHOW TABLES;")
-        tables = cursor.fetchall()
-    except Error as e:
-        print(f"Error: {e}")
-    return [table[0] for table in tables]
-
-@app.route('/mysql_db_update', methods=['POST'])
-def mysql_db_update():
-    sql_file_path = './mysite/rtc_project_backend/pstu_rtc.sql'
-    conn = get_db()
-    cursor = conn.cursor()
-    tables = show_tables(conn, cursor)
-    drop_tables(conn, cursor)
-    import_sql_file(conn, cursor, sql_file_path)
-    return jsonify(tables=tables), 200
-
-
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000 , debug=True) # for docker only
-    app.run(debug=True)
+    app.run(debug=False)
--- db.py
+++ db.py
@@ -1,19 +1,29 @@
 import mysql.connector
 
 # for pythonanywhere server
+# db_config = {
+#     'host': 'rakib73.mysql.pythonanywhere-services.com',
+#     'user': 'rakib73',
+#     'password': 'rakib73rakib73',
+#     'database': 'rakib73$final_project'
+# }
+
 db_config = {
-    'host': 'rakib73.mysql.pythonanywhere-services.com',
-    'user': 'rakib73',
-    'password': 'rakib73rakib73',
-    'database': 'rakib73$final_project'
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'rakib',
+    'database': 'pstu_rtc'
 }
 
+
 # db_config = {
-#     'host': 'localhost',
-#     'user': 'root',
-#     'password': '',
+#     'host': '0.tcp.in.ngrok.io',
+#     'port': 12196,  # The dynamic port provided by ngrok
+#     'user': 'root',  # Same as your local MySQL user
+#     'password': '',  # Same as your local MySQL password
 #     'database': 'pstu_rtc'
 # }
+
 
 # Function to establish a database connection
 
